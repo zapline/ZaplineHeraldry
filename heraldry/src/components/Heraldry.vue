82d8351ccc241bd81216cd44364f7f67576f0a37
zapline <278998871@qq.com>
2018-05-18 20:19:20 +0800

    * HelloWorld -> Heraldry

---
+++ heraldry/src/components/Heraldry.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Heraldry</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Heraldry',
+  data () {
+    return {
+      msg: 'Welcome to Heraldry'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- heraldry/src/router/index.js
+++ heraldry/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Heraldry from '@/components/Heraldry'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Heraldry',
+      component: Heraldry
     }
   ]
 })
